890177ab2eac2eaaa750bdaa1d95b553722e186c
Kazol <razbongshikazol@gmail.com>
2018-06-07 14:04:47 +0600

    fonder member added in homepage

--- resources/views/home_page/founder_member.blade.php
+++ resources/views/home_page/founder_member.blade.php
@@ -3,7 +3,288 @@
 <div class='main-content'>
   <div class='main-content-wrap'>
     <h1 class="content-header">Founder member:</h1>
+    <!-- Start of Founder Member List -->
+                    <div class='founder-member'>
+                      <div class='row'>
+                        <div class='col-md-4'>
+                          <img class="member-img img-responsive" src="{{asset('home_page_assets/img/Mohit.jpg')}}" alt="Mohit">
+                          <h4 class='member-role'>admin</h4>
+                        </div>
+                        <div class='col-md-8'>
+                          <div class='member-discription'>
+                            <div class='member-name'>
+                              <span class='member-title'>Name:</span>
+                              <span>Mohit Rahman Chowdhury</span>
+                            </div>
+                            <div class='member-qualification'>
+                              <span class='member-title'>Educational Qualifications: </span>
+                              <span>B.B.S.(Honours), LL.B.</span>
+                            </div>
+                            <div class='member-occupation'>
+                              <span class='member-title'>Occupation: </span>
+                              <span>Business And Income Tax & Company Law Adviser</span>
+                            </div>
+                            <div class='member-activity'>
+                              <span class='member-title'>Extra Curricular Activity : </span>
+                              <ol>
+                                <li>President (2008-2009), Leo Club of Narayang City District 315A2, Bangladesh.</li>
+                                <li>Zone Director (2011-20012) Leo District Council, District 315A2, Bangladesh</li>
+                                <li>Founder Director - Lions Club Of  Dhaka Cambrian.</li>
+                                <li>Life Member, Bangladesh Red Crescent Society.</li>
+                                <li>Member, Dhaka Taxes Bar Association. </li>
+                              </ol>
+                            </div>
+                            <div class='member-address'>
+                              <span class='member-title'>Address: </span>
+                              <span>House 119,Road 7, Block D,Bashundhara R/A, Dhaka.</span>
+                            </div>
+                            <div class='member-email'>
+                              <span class='member-title'>Email: </span>
+                              <span>mohitrahmanc@gmail.com</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class='row'>
+                        <div class='col-md-4'>
+                          <img class="member-img img-responsive" src="{{asset('home_page_assets/img/Tonmoy.jpg')}}" alt="Tonmoy">
+                          <h4 class='member-role'>Admin</h4>
+                        </div>
+                        <div class='col-md-8'>
+                          <div class='member-discription'>
+                            <div class='member-name'>
+                              <span class='member-title'>Name:</span>
+                              <span>Ovishak Hossain Tonmoy </span>
+                            </div>
+                            <div class='member-qualification'>
+                              <span class='member-title'>Educational Qualifications: </span>
+                              <span>MBBS,DA</span>
+                            </div>
+                            <div class='member-occupation'>
+                              <span class='member-title'>Occupation: </span>
+                              <span>Anaesthesiologists</span>
+                            </div>
+                            <div class='member-activity'>
+                              <span class='member-title'>Extra Curricular Activity : </span>
+                              <ol>
+                                <li>Central advisor Sandhani </li>
+                              </ol>
+                            </div>
+                            <div class='member-address'>
+                              <span class='member-title'>Address: </span>
+                              <span>Institute of child and mother health. Matuail,Dhaka.</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class='row'>
+                        <div class='col-md-4'>
+                          <img class="member-img img-responsive" src="{{asset('home_page_assets/img/Rakib.jpg')}}" alt="Rakib">
+                          <h4 class='member-role'>Moderator</h4>
+                        </div>
+                        <div class='col-md-8'>
+                          <div class='member-discription'>
+                            <div class='member-name'>
+                              <span class='member-title'>Name:</span>
+                              <span>Md. Rakib Uddin </span>
+                            </div>
+                            <div class='member-qualification'>
+                              <span class='member-title'>Educational Qualifications: </span>
+                              <span>B.Com</span>
+                            </div>
+                            <div class='member-occupation'>
+                              <span class='member-title'>Occupation: </span>
+                              <span>Business</span>
+                            </div>
+                            <div class='member-activity'>
+                              <span class='member-title'>Social Activity : </span>
+                              <ol>
+                                <li>Founder & Charter President (2004-2005),Leo Club of Narayanganj Swarnali ,District 315 A2,Bangladesh.</li>
+                                <li>District President (2012-2013),Leo District Council, District 315A2, Bangladesh.</li>
+                                <li>Founder Excutive President, Sayed Ali Sarder-Ajirun Nesa Memorial Foundation.</li>
+                                <li>Life Member, Bangladesh Red Crescent Society.</li>
+                              </ol>
+                            </div>
+                            <div class='member-address'>
+                              <span class='member-title'>Address: </span>
+                              <span>34 K.B.Saha Road,Amlapara,Narayanganj-1400</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class='row'>
+                        <div class='col-md-4'>
+                          <img class="member-img img-responsive" src="{{asset('home_page_assets/img/Sabbir.jpg')}}" alt="Sabbir">
+                          <h4 class='member-role'>Moderator</h4>
+                        </div>
+                        <div class='col-md-8'>
+                          <div class='member-discription'>
+                            <div class='member-name'>
+                              <span class='member-title'>Name:</span>
+                              <span>Sabbir Hossain</span>
+                            </div>
+                            <div class='member-qualification'>
+                              <span class='member-title'>Educational Qualifications: </span>
+                              <span>B.B.S.(Honours)</span>
+                            </div>
+                            <div class='member-occupation'>
+                              <span class='member-title'>Occupation: </span>
+                              <span>Business</span>
+                            </div>
+                            <div class='member-address'>
+                              <span class='member-title'>Address: </span>
+                              <span>Narayanganj</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class='row'>
+                        <div class='col-md-4'>
+                          <img class="member-img img-responsive" src="{{asset('home_page_assets/img/Samarjit-Mitu.jpg')}}" alt="Samarjit">
+                          <h4 class='member-role'>Moderator</h4>
+                        </div>
+                        <div class='col-md-8'>
+                          <div class='member-discription'>
+                            <div class='member-name'>
+                              <span class='member-title'>Name:</span>
+                              <span>Samarjit Chakrabarty</span>
+                            </div>
+                            <div class='member-qualification'>
+                              <span class='member-title'>Educational Qualifications: </span>
+                              <span>BBA</span>
+                            </div>
+                            <div class='member-occupation'>
+                              <span class='member-title'>Occupation: </span>
+                              <span>Business</span>
+                            </div>
+                            <div class='member-address'>
+                              <span class='member-title'>Address: </span>
+                              <span>56 S M Maleh Road, Tanbazar,  Narayanganj</span>
+                            </div>
+                            <div class='member-email'>
+                              <span class='member-title'>Email: </span>
+                              <span>sammitu16@gmail.com</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class='row'>
+                        <div class='col-md-4'>
+                          <img class="member-img img-responsive" src="{{asset('home_page_assets/img/Manshi.jpg')}}" alt="Manashi">
+                          <h4 class='member-role'>Moderator</h4>
+                        </div>
+                        <div class='col-md-8'>
+                          <div class='member-discription'>
+                            <div class='member-name'>
+                              <span class='member-title'>Name:</span>
+                              <span>Manashi Adhikary</span>
+                            </div>
+                            <div class='member-qualification'>
+                              <span class='member-title'>Educational Qualifications: </span>
+                              <span>MBA( Banking and Insurance)</span>
+                            </div>
+                            <div class='member-occupation'>
+                              <span class='member-title'>Occupation: </span>
+                              <span>Teacher</span>
+                            </div>
+                            <div class='member-address'>
+                              <span class='member-title'>Address: </span>
+                              <span>56 S M Maleh Road, Tanbazar,  Narayanganj</span>
+                            </div>
+                            <div class='member-email'>
+                              <span class='member-title'>Email: </span>
+                              <span>manshi14@gmail.com</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class='row'>
+                        <div class='col-md-4'>
+                          <img class="member-img img-responsive" src="{{asset('home_page_assets/img/Priyanka.jpg')}}" alt="Priyanka">
+                          <h4 class='member-role'>Moderator</h4>
+                        </div>
+                        <div class='col-md-8'>
+                          <div class='member-discription'>
+                            <div class='member-name'>
+                              <span class='member-title'>Name:</span>
+                              <span>Priyanka Bhattacharjee</span>
+                            </div>
+                            <div class='member-qualification'>
+                              <span class='member-title'>Educational Qualifications: </span>
+                              <span>MBBS (DU) , Diploma in Medical diagnostic ultrasound ( SUB)</span>
+                            </div>
+                            <div class='member-occupation'>
+                              <span class='member-title'>Occupation: </span>
+                              <span>Sonologist , Labaid diagnostic Limited Narayanganj Mediplus Medical Services Narayanganj </span>
+                            </div>
+                            <div class='member-address'>
+                              <span class='member-title'>Address: </span>
+                              <span>Narayanganj</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class='row'>
+                        <div class='col-md-4'>
+                          <img class="member-img img-responsive" src="{{asset('home_page_assets/img/Jamil.jpg')}}" alt="Jamil">
+                          <h4 class='member-role'>Moderator</h4>
+                        </div>
+                        <div class='col-md-8'>
+                          <div class='member-discription'>
+                            <div class='member-name'>
+                              <span class='member-title'>Name:</span>
+                              <span>: S.M. Ahsan Ali Jamil</span>
+                            </div>
+                            <div class='member-qualification'>
+                              <span class='member-title'>Educational Qualifications: </span>
+                              <span>M.S.S</span>
+                            </div>
+                            <div class='member-occupation'>
+                              <span class='member-title'>Occupation: </span>
+                              <span>Private Service, Sales Admin n Audit</span>
+                            </div>
+                            <div class='member-address'>
+                              <span class='member-title'>Address: </span>
+                              <span>Darussalam, Dhaka</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class='row'>
+                        <div class='col-md-4'>
+                          <img class="member-img img-responsive" src="{{asset('home_page_assets/img/Fahad.jpg')}}" alt="Fahad">
+                          <h4 class='member-role'>Moderator</h4>
+                        </div>
+                        <div class='col-md-8'>
+                          <div class='member-discription'>
+                            <div class='member-name'>
+                              <span class='member-title'>Name:</span>
+                              <span>Fahad Naim (Sezan)</span>
+                            </div>
+                            <div class='member-qualification'>
+                              <span class='member-title'>Educational Qualifications: </span>
+                              <span>B.SC (Honours), Apparel Manufacturing Technology, BUFT</span>
+                            </div>
+                            <div class='member-occupation'>
+                              <span class='member-title'>Occupation: </span>
+                              <span>Business (Buying House)</span>
+                            </div>
+                            <div class='membership'>
+                              <span class='member-title'>Member: </span>
+                              <span>BGMEA</span>
+                            </div>
+                            <div class='member-address'>
+                              <span class='member-title'>Address: </span>
+                              <span>: 37 North Chashara, Narayanganj-1400</span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- End of Founder Member List -->
   </div>
+
+
   <footer id="site-footer">
     <div class="row">
       <div class="col-sm-6">
